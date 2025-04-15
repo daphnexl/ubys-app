873424fa7865fc4eb58c7ade6d5a686cd28adca7
Defne Çivrilli <119342607+daphnexl@users.noreply.github.com>
2025-04-15 17:17:42 +0300

    Login Butonu Eklendi

--- ubys-app/MVVM/ViewModel/LoginPageViewModel.cs
+++ ubys-app/MVVM/ViewModel/LoginPageViewModel.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ubys_app.MVVM.ViewModel
 {
-    class LoginPageViewModel
+    public partial class LoginPageViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string username;
+
+        [ObservableProperty]
+        private string password;
+
+        [RelayCommand]
+        private void Login()
+        {
+            // Basit giriş kontrolü – örnek amaçlı
+            if (Username == "admin" && Password == "1234")
+            {
+                MessageBox.Show("Login successful!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                // Rolüne göre yönlendirme burada yapılabilir.
+            }
+            else
+            {
+                MessageBox.Show("Invalid credentials.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        [RelayCommand]
+        private void ForgotPassword()
+        {
+            MessageBox.Show("Redirecting to password recovery...", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
     }
 }
