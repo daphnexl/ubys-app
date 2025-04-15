87b2becf9bb724fc144089865355ab80976a5b9e
Defne Çivrilli <119342607+daphnexl@users.noreply.github.com>
2025-04-15 17:32:51 +0300

    CommandBase

---
+++ ubys-app/Commands/CommandBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Input;
+
+public abstract class CommandBase : ICommand
+{
+    public event EventHandler CanExecuteChanged;
+
+    public abstract bool CanExecute(object parameter);
+    public abstract void Execute(object parameter);
+
+    protected void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
