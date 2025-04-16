4c2fd78315a0a4753a350c7110129c4f1287e7b9
irmakoflu <irmakoflu@icloud.com>
2025-04-16 20:15:29 +0300

    modeller oluşturuldu

---
+++ ubys-app/MVVM/Model/Teachers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ubys_app.MVVM.Model
+{
+    public class Teachers
+    {
+        [Key]
+        [ForeignKey(nameof(User))]
+        public int Teacher_id { get; set; }
+
+        [Required]
+        public string Department { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ ubys-app/MVVM/Model/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ubys_app.MVVM.Model
+{
+    [Index(nameof(LoginNumber), IsUnique = true)]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Firstname { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Roles))]
+        public int Roles { get; set; }
+
+        [Required]
+      
+        public int LoginNumber { get; set; }
+
+        [Required]
+        public string HashPassword { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        public Roles RoleNavigation { get; set; }
+
+    }
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2,
+        Other = 3
+    }
+}
