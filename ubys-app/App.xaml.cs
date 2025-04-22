333353e4d423c34c476b5431c702af2db0102092
irmakoflu <irmakoflu@icloud.com>
2025-04-22 16:50:38 +0300

    Database başlatma

--- ubys-app/App.xaml.cs
+++ ubys-app/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.IO;
 using System.Windows;
 using ubys_app.Data;
+using ubys_app.MVVM.ViewModel;
 using ubys_app.Services;
 
 namespace ubys_app
@@ -15,13 +16,13 @@ namespace ubys_app
     public partial class App : Application
     {
         private readonly IServiceProvider serviceProvider;
-    
-    public App()
+
+        public App()
         {
             IServiceCollection services = new ServiceCollection();
 
-            string dbPath = Path.Combine(Directory.GetCurrentDirectory(), "AppDatabase.db3");
-            services.AddDbContext<AppDbContext>(options=> options.UseSqlite($"Data source={dbPath}"));
+            string dbPath = Path.Combine(Directory.GetCurrentDirectory(), "ubysDatabase.db3");
+            services.AddDbContext<AppDbContext>(options => options.UseSqlite($"Data source={dbPath}"));
             services.AddScoped<StudentService>();
             services.AddScoped<TeacherService>();
             services.AddScoped<AdminService>();
@@ -31,9 +32,35 @@ namespace ubys_app
             services.AddScoped<SemesterService>();
             services.AddScoped<RoleService>();
             services.AddScoped<EnrollmentService>();
-         
+
+
+services.AddSingleton<MainVM>();
+
+            services.AddSingleton<MainWindow>(s => new MainWindow()
+            {
+                DataContext=s.GetRequiredService<MainWindow>()
+            });
+            serviceProvider = services.BuildServiceProvider();
+            EnsureDataBaseCreated();
+
+
         }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow=serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+        private void EnsureDataBaseCreated()
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+
+                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                context.Database.EnsureCreated();
+            }
 
 
+        }
     }
 }
--- ubys-app/obj/Debug/net8.0-windows/Components/NavigationBarStudent.g.i.cs
+++ ubys-app/obj/Debug/net8.0-windows/Components/NavigationBarStudent.g.i.cs
@@ -53,7 +53,7 @@ namespace ubys_app.Components {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ubys-app;V1.0.0.0;component/components/navigationbarstudent.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ubys-app;component/components/navigationbarstudent.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Components\NavigationBarStudent.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
