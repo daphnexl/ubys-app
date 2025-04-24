a6fb5066f5c293a459097c92d2bbf39fba6ea6b3
Defne Çivrilli <119342607+daphnexl@users.noreply.github.com>
2025-04-24 11:17:18 +0300

    xaml design bug fixes

--- ubys-app/obj/Debug/net8.0-windows10.0.26100.0/MainWindow.g.i.cs
+++ ubys-app/obj/Debug/net8.0-windows10.0.26100.0/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace ubys_app {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ubys-app;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ubys-app;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
