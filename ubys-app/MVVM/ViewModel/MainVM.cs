975175832afaf6f8a9fc6fccf8dae03bfa0699bb
Defne Çivrilli <119342607+daphnexl@users.noreply.github.com>
2025-04-25 15:38:12 +0300

    MainVM update

--- ubys-app/MVVM/ViewModel/MainVM.cs
+++ ubys-app/MVVM/ViewModel/MainVM.cs
@@ -1,25 +1,23 @@
 ﻿using System;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.ComponentModel;
 using ubys_app.Stores;
 
 namespace ubys_app.MVVM.ViewModel
 {
-    public class MainVM : ViewModelBase 
+    public class MainVM : ViewModelBase
     {
         private readonly NavigationStore _navigationStore;
         private readonly ModalNavigationStore _modalNavigationStore;
-
         public ViewModelBase CurrentViewModel => _navigationStore.CurrentViewModel;
         public ViewModelBase CurrentModalViewModel => _modalNavigationStore.CurrentViewModel;
+
         public bool IsOpen => _modalNavigationStore.IsOpen;
 
         public MainVM(NavigationStore navigationStore, ModalNavigationStore modalNavigationStore)
         {
-            _navigationStore = navigationStore ?? throw new ArgumentNullException(nameof(navigationStore)); // null kontrolü eklendi
-            _modalNavigationStore = modalNavigationStore ?? throw new ArgumentNullException(nameof(modalNavigationStore)); // null kontrolü eklendi
+
+            _navigationStore = navigationStore ?? throw new ArgumentNullException(nameof(navigationStore));
+            _modalNavigationStore = modalNavigationStore ?? throw new ArgumentNullException(nameof(modalNavigationStore));
 
             _navigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
             _modalNavigationStore.CurrentViewModelChanged += OnCurrentModalViewModelChanged;
@@ -27,13 +25,20 @@ namespace ubys_app.MVVM.ViewModel
 
         private void OnCurrentViewModelChanged()
         {
-            OnPropertyChanged(nameof(CurrentViewModel)); 
+            OnPropertyChanged(nameof(CurrentViewModel));
         }
 
         private void OnCurrentModalViewModelChanged()
         {
             OnPropertyChanged(nameof(CurrentModalViewModel));
-            OnPropertyChanged(nameof(IsOpen)); 
+            OnPropertyChanged(nameof(IsOpen));
+        }
+        public override void Dispose()
+        {
+            _navigationStore.CurrentViewModelChanged -= OnCurrentViewModelChanged;
+            _modalNavigationStore.CurrentViewModelChanged -= OnCurrentModalViewModelChanged;
+
+            base.Dispose();
         }
     }
 }
