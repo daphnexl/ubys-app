67419b8c83574bdc0cb944a7f7f6a658095075d0
irmakoflu <irmakoflu@icloud.com>
2025-05-07 15:22:47 +0300

    assets eklendi resource yapıldı

--- ubys-app/Components/NavigationTeacherBar.xaml.cs
+++ ubys-app/Components/NavigationTeacherBar.xaml.cs
@@ -24,5 +24,10 @@ namespace ubys_app.Components
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
